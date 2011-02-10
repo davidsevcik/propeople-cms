3cafa5375aafc3d6a6c6946ee19c0f437e27202f
davidsevcik <david.sevcik@gmail.com>
2011-02-10 16:49:28 +0100

    script for mass thumbnails

---
+++ set_category_images.rb
@@ -0,0 +1,19 @@
+require 'action_controller'
+require 'action_controller/test_process.rb'
+
+
+Page.find_all_by_class_name('ProductPage').attachments.each do |att|
+  temp_file = att.temp_path || att.create_temp_file
+  att.create_or_update_thumbnail(temp_file, 'product_perex', 'b70x90')
+  temp_file = att.temp_path || att.create_temp_file
+  att.create_or_update_thumbnail(temp_file, 'product_category', 'b179x133')
+end
+
+Page.find_all_by_class_name('ProductCategoriesPage').each do |page|
+  if product = page.descendants.detect {|desc| desc.class_name == 'ProductPage'}
+    if attachment = product.attachments[0]
+      file = ActionController::TestUploadedFile.new("public" + attachment.public_filename, attachment.content_type)
+      page.attachments.create(:uploaded_data => file)
+    end
+  end
+end
