ba894f50502a6babfc5cbb5553152fc8b811c9f4
davidsevcik <david.sevcik@gmail.com>
2011-02-10 16:52:45 +0100

    script for mass thumbnails

--- set_category_images.rb
+++ set_category_images.rb
@@ -2,11 +2,13 @@ require 'action_controller'
 require 'action_controller/test_process.rb'
 
 
-Page.find_all_by_class_name('ProductPage').attachments.each do |att|
-  temp_file = att.temp_path || att.create_temp_file
-  att.create_or_update_thumbnail(temp_file, 'product_perex', 'b70x90')
-  temp_file = att.temp_path || att.create_temp_file
-  att.create_or_update_thumbnail(temp_file, 'product_category', 'b179x133')
+Page.find_all_by_class_name('ProductPage').each |page| 
+  page.attachments.each do |att|
+    temp_file = att.temp_path || att.create_temp_file
+    att.create_or_update_thumbnail(temp_file, 'product_perex', 'b70x90')
+    temp_file = att.temp_path || att.create_temp_file
+    att.create_or_update_thumbnail(temp_file, 'product_category', 'b179x133')
+  end
 end
 
 Page.find_all_by_class_name('ProductCategoriesPage').each do |page|
